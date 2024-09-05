c2dd9056d81cb3217bfe4e0a80455b2097f763cf
lucky singh <singhkarthik174@gmail.com>
2024-09-05 14:38:33 +0530

    added suspense

--- components/home/GlobalSearch.tsx
+++ components/home/GlobalSearch.tsx
@@ -1,18 +1,21 @@
 import { SearchIcon } from "lucide-react";
-import React from "react";
+import React, { Suspense, ReactNode } from "react";
 import { motion } from "framer-motion";
 import { Input } from "../ui/input";
-import { useSearchParams, usePathname, useRouter } from "next/navigation";
+import { useSearchParams, usePathname, useRouter, ReadonlyURLSearchParams } from "next/navigation";
+
+const SearchParamsWrapper = ({ children }: { children: (searchParams: ReadonlyURLSearchParams) => ReactNode }) => {
+  const searchParams = useSearchParams();
+  return children(searchParams);
+};
 
 const GlobalSearch = () => {
-  const searchParams = useSearchParams();
   const pathname = usePathname();
   const { replace } = useRouter();
 
-
-  const handleSearch = (term: string) => {
+  const handleSearch = (term: string, searchParams: ReadonlyURLSearchParams) => {
     const params = new URLSearchParams(searchParams.toString());
-  if (term) {
+    if (term) {
       params.set('query', term);
     } else {
       params.delete('query');
@@ -20,27 +23,32 @@ const GlobalSearch = () => {
     replace(`${pathname}?${params.toString()}`);
   }
 
-
   return (
-    <motion.form
-      className="relative hidden w-full max-w-md md:flex"
-      initial={{ opacity: 0, y: -20 }}
-      animate={{ opacity: 1, y: 0 }}
-      transition={{ duration: 0.5, delay: 0.1 }}
-    >
-      <div className="absolute left-2.5 top-2.5 h-4 w-4 text-muted-foreground">
-        <SearchIcon className="h-4 w-4" />
-      </div>
-      <Input
-        type="search"
-        placeholder="Search..."
-        className="w-full rounded-md pl-8"
-        onChange={(e) => {
-          handleSearch(e.target.value);
-        }}
-        defaultValue={searchParams.get('query')?.toString()}
-      />
-    </motion.form>
+    <Suspense fallback={<div>Loading...</div>}>
+      <SearchParamsWrapper>
+        {(searchParams) => (
+          <motion.form
+            className="relative hidden w-full max-w-md md:flex"
+            initial={{ opacity: 0, y: -20 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.5, delay: 0.1 }}
+          >
+            <div className="absolute left-2.5 top-2.5 h-4 w-4 text-muted-foreground">
+              <SearchIcon className="h-4 w-4" />
+            </div>
+            <Input
+              type="search"
+              placeholder="Search..."
+              className="w-full rounded-md pl-8"
+              onChange={(e) => {
+                handleSearch(e.target.value, searchParams);
+              }}
+              defaultValue={searchParams.get('query') ?? ''}
+            />
+          </motion.form>
+        )}
+      </SearchParamsWrapper>
+    </Suspense>
   );
 };
 
